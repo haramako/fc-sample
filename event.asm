5bb5c5f693e333a7cc21701c071f509304b6e403
Makoto Harada <haramako@gmail.com>
2013-10-18 20:15:53 +0900

    fix add module.

--- event.asm
+++ event.asm
@@ -7,7 +7,7 @@ _event_irq_setup:
 	ldx _mmc3_cbank_bak+1
 	mmc3_cbank #1
 	
-	loadw _irq_next, event_irq_1
+	loadw _ppu_irq_next, event_irq_1
 	rts
 
 ;;; IRQ割り込み(上辺)
@@ -20,19 +20,19 @@ event_irq_1:
 	jsr scroll
 
 	lda #%10100001
-	sta _PPU_CTRL1
+	sta _nes_PPU_CTRL1
 
-	ldx #(_CBANK_TEXT+0)
+	ldx #(_common_CBANK_TEXT+0)
 	mmc3_cbank #0
-	ldx #(_CBANK_TEXT+1)
+	ldx #(_common_CBANK_TEXT+1)
 	mmc3_cbank #1
 
-	loadw _irq_next, event_irq_2
+	loadw _ppu_irq_next, event_irq_2
 	rts
 
 ;;; IRQ割り込み(下辺)
 event_irq_2:
-	sta _MMC3_IRQ_DISABLE
+	sta _mmc3_IRQ_DISABLE
 	xwait #4
 	nop
 
@@ -45,6 +45,6 @@ event_irq_2:
 	mmc3_cbank #1
 
 	lda _ppu_ctrl1_bak
-	sta _PPU_CTRL1
+	sta _nes_PPU_CTRL1
 
 	rts
